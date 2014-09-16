d6aa0954f7ee2d25f927c1466ccfda9c29bcef4b
zanninna <tabechi155@gmail.com>
2014-09-16 17:44:33 +0700

    Reset Method of skillOne

--- src/tablemonsgame/MoveEmpty.java
+++ src/tablemonsgame/MoveEmpty.java
@@ -5,13 +5,42 @@ import org.newdawn.slick.SlickException;
 
 public class MoveEmpty {
 	private Image skill_Empty;
+	private float x;
+	private float y;
+	private float vx;
+	private float vy;
 	
-	public MoveEmpty() throws SlickException {
+	public MoveEmpty(int x, int y) throws SlickException {
+		this.x = x;
+		this.y = y;
+		this.vx = x;
+		this.vy = y;
 		skill_Empty =  new Image("res/empty.png");
 	}
 
 	public void render() {
-		skill_Empty.draw(350,550);
+		skill_Empty.draw(x,y);
+	}
+
+	public void up() {
+		vy -= 100;
+	}
+
+	public void down() {
+		vy += 100;
+	}
+
+	public void left() {
+		vx -= 100;
+	}
+
+	public void right() {
+		vx += 100;
+	}
+
+	public void update() {
+		x = vx;
+		y = vy;
 	}
 	
 	
--- src/tablemonsgame/TableMonsters.java
+++ src/tablemonsgame/TableMonsters.java
@@ -4,14 +4,16 @@ import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 
 public class TableMonsters extends BasicGame{
 
 	public static final int GAME_WIDTH = 600;
 	public static final int GAME_HEIGHT = 700;
-	private Skill skill;
+	private Skill[] skillOne = new Skill [9];
 	private MoveEmpty empty;
+	
 
 	public TableMonsters(String title) {
 		super(title);
@@ -19,20 +21,60 @@ public class TableMonsters extends BasicGame{
 
 	@Override
 	public void render(GameContainer container, Graphics g) throws SlickException {
-		skill.render();
+		renderSkillOne();
 		empty.render();
-		
+	}
+
+	private void renderSkillOne() {
+		skillOne[0].render();
+		skillOne[1].render();
+		skillOne[2].render();
+		skillOne[3].render();
+		skillOne[4].render();
+		skillOne[5].render();
+		skillOne[6].render();
+		skillOne[7].render();
 	}
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		skill = new Skill();
-		empty = new MoveEmpty();
+		initSkillOne();
+		empty = new MoveEmpty(350,550);
+	}
+
+	private void initSkillOne() throws SlickException {
+		skillOne[0] = new Skill(150,350,0);
+		skillOne[1] = new Skill(250,350,1);
+		skillOne[2] = new Skill(350,350,2);
+		skillOne[3] = new Skill(150,450,3);
+		skillOne[4] = new Skill(250,450,4);
+		skillOne[5] = new Skill(350,450,5);
+		skillOne[6] = new Skill(150,550,6);
+		skillOne[7] = new Skill(250,550,7);
+		skillOne[8] = new Skill(350,550,8);
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
+		empty.update();
 	}
+	
+	@Override
+	public void keyPressed(int key, char c){
+		if(key == Input.KEY_UP){
+			empty.up();
+		}
+		if(key == Input.KEY_DOWN){
+			empty.down();
+		}
+		if(key == Input.KEY_LEFT){
+			empty.left();
+		}
+		if(key == Input.KEY_RIGHT){
+			empty.right();
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 	    try {
